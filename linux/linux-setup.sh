c692b35b69b9d8d3cfcbbc2f5fec4be951b7adb0
Grant Windes <gwindes@gmail.com>
2017-03-04 00:13:51 -0800

    Fixed echo statement & added htop to apt-get

--- linux/linux-setup.sh
+++ linux/linux-setup.sh
@@ -1,3 +1,6 @@
+# Setup bash aliases to source from .profile
+echo 'source ~/.bash_aliases' >> .profile
+
 #Setup Python Libs & PIP
 wget https://bootstrap.pypa.io/get-pip.py
 sudo pip install bs4
@@ -6,3 +9,4 @@ sudo pip install requests
 # apt-get packages
 sudo apt-get install arp-scan
 sudo apt-get install wakeonlan
+sudo apt-get install htop
