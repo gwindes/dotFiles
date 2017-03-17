7a797b711208d603095483639e87b91c5e98dee0
Grant Windes <gwindes@users.noreply.github.com>
2017-03-16 23:39:16 -0700

    Cleanup pip file & auto -y to apt-get install

--- linux/linux-setup.sh
+++ linux/linux-setup.sh
@@ -7,8 +7,10 @@ wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 sudo pip install bs4
 sudo pip install requests
+rm get-pip.py #cleanup
 
 # apt-get packages
-sudo apt-get install arp-scan
-sudo apt-get install wakeonlan
-sudo apt-get install htop
+sudo apt-get install -y arp-scan
+sudo apt-get install -y wakeonlan
+sudo apt-get install -y htop
+sudo apt-get install -y synergy
