75205a765e952610119daa458221486d52bff6d1
Grant Windes <gwindes@users.noreply.github.com>
2017-05-24 11:02:53 -0700

    Added miniconda

--- linux/linux-setup.sh
+++ linux/linux-setup.sh
@@ -14,3 +14,7 @@ sudo apt-get install -y arp-scan
 sudo apt-get install -y wakeonlan
 sudo apt-get install -y htop
 sudo apt-get install -y synergy
+
+# setup MiniConda py2.7
+wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+bash Miniconda2-latest-Linux-x86_64.sh
