185f63f3968011d8902298e26538e031ec3cf8da
Grant Windes <gwindes@users.noreply.github.com>
2017-08-08 22:31:41 -0700

    Added sublimeText 3 and MiniConda Py3

--- linux/linux-setup.sh
+++ linux/linux-setup.sh
@@ -19,3 +19,15 @@ sudo apt-get install -y openssh-server
 # setup MiniConda py2.7
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 bash Miniconda2-latest-Linux-x86_64.sh
+rm Miniconda2-latest-Linux-x86_64.sh
+
+# setup MiniConda py3
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
+
+# Setup SublimeText 3
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install -y sublime-text
