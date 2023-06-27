6edf4a6ac13051cbe0e43b71b11fd3c5e11f8a74
Grant Windes <gwindes@users.noreply.github.com>
2023-06-27 11:08:21 -0700

    Update linux-setup.sh
    Updated linux setup script by removing python 2, updated to sublimetext4

--- linux/linux-setup.sh
+++ linux/linux-setup.sh
@@ -2,33 +2,17 @@
 echo 'source ~/.bash_aliases' >> .profile
 source ~/.bash_aliases
 
-#Setup Python Libs & PIP
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
-sudo pip install bs4
-sudo pip install requests
-rm get-pip.py #cleanup
 
 # apt-get packages
 sudo apt-get install -y arp-scan
 sudo apt-get install -y wakeonlan
 sudo apt-get install -y htop
-sudo apt-get install -y synergy
 sudo apt-get install -y openssh-server
 sudo apt-get install -y git
 
-# setup MiniConda py2.7
-wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
-bash Miniconda2-latest-Linux-x86_64.sh
-rm Miniconda2-latest-Linux-x86_64.sh
 
-# setup MiniConda py3
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
-rm Miniconda3-latest-Linux-x86_64.sh
-
-# Setup SublimeText 3
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+# Setup SublimeText 4
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install -y sublime-text
